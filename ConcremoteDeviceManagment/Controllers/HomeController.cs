a67feed111538377ccc78cbc87eb73ee3855e975
RolandDfa <roland.huijskes@planet.nl>
2018-01-25 10:16:56 +0100

    Added PatrialView

--- ConcremoteDeviceManagment/Controllers/HomeController.cs
+++ ConcremoteDeviceManagment/Controllers/HomeController.cs
@@ -15,25 +15,17 @@ namespace ConcremoteDeviceManagment.Controllers
         //List<Stock> Stock = new List<Stock>();
         // List<Pricelist> StockInfo = new List<Pricelist>();
         public ActionResult Index(int? SelectedDevice)
-        {
-          //  var DeviceType = db.DeviceType.OrderBy(q => q.name).ToList().OrderBy(x => x.device_type_id);
-        //    ViewBag.SelectedDevice = new SelectList(DeviceType, "device_type_id", "name", SelectedDevice);
-            //   var StockVM = from d in db.DeviceConfig
-            //               select d;
-            // foreach (var item in SelectedDevice)
-               List<SelectListItem> query = db.DeviceType.OrderBy(c => c.name).Select(c => new SelectListItem { Text = c.name, Value = c.name}).ToList();
+        {      
+            List<SelectListItem> query = db.DeviceType.OrderBy(c => c.name).Select(c => new SelectListItem { Text = c.name, Value = c.name}).ToList();
             ViewBag.SelectedDevice = query;
             return View();
         }
-        public PartialViewResult GetDevice(int device_config_id)
+        public PartialViewResult GetDevice(string device_type_id)
         {
-            var model = db.DeviceConfig.Find(device_config_id);
+            var model = from d in db.DeviceConfig
+                         select d;
+           // var model = db.DeviceConfig.Find(device_type_id);
             return PartialView("PartialView", model);
         }
-        //public PartialViewResult GetDevice(int device_type_id)
-        //{
-        //    var model = db.DeviceType.Find(device_type_id);
-        //    return PartialView("PartialView", model);
-        //}
     }
 }
