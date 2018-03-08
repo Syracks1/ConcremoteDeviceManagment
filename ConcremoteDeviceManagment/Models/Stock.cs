08794bdca6370fdb33861f4654200cc765e3d187
LUCA classens <lclassens@hotmail.com>
2018-03-08 09:08:11 +0100

    Fixed relations kinda maybe 2.0

--- ConcremoteDeviceManagment/Models/Stock.cs
+++ ConcremoteDeviceManagment/Models/Stock.cs
@@ -101,20 +101,41 @@ namespace ConcremoteDeviceManagment.Models
             public int id { get; set; }
             public bool Active { get; set; }
         }
-        [Table("Devicestatus")]
+
+    //[Table("AspNetUserRoles")]
+    //public class AspNetUserRoles
+    //{
+    //    [Key]
+    //    [Column(Order = 0)]
+    //    public string UserId { get; set; }
+    //    [Key]
+    //    [Column(Order = 1)]
+    //    public string RoleId { get; set; }
+    //    [ForeignKey("UserId")]
+    //    public virtual AspNetUsers AspNetUsers { get; set; }
+    //    [ForeignKey("RoleId")]
+    //    public virtual AspNetRoles AspNetRoles { get; set; }
+    //}
+
+    [Table("Devicestatus")]
         public class DeviceStatus
         {
         [Key]
+        [Column(Order = 0)]
         public int id { get; set; }
-            public int ConcremoteDevice_id { get; set; }
-            public int Device_statustypes_id { get; set; }
-            public string Employee_1 { get; set; }
-            public string Employee_2 { get; set; }
-            public DateTime Sign_Date { get; set; }
-            public virtual DeviceConfig DeviceConfig { get; set; }
-            public virtual ConcremoteDevice ConcremoteDevice { get; set; }
-            public virtual Device_statustypes Device_Statustypes { get; set; }
-        }
+        public int Device_statustypes_id { get; set; }
+        public int ConcremoteDevice_id { get; set; }
+        public int DeviceConfig_id { get; set; }
+        public string Employee_1 { get; set; }
+        public string Employee_2 { get; set; }
+        public DateTime Sign_Date { get; set; }
+        [ForeignKey("DeviceConfig_id")]
+        public virtual DeviceConfig DeviceConfig { get; set; }
+        [ForeignKey("ConcremoteDevice_id")]
+        public virtual ConcremoteDevice ConcremoteDevice { get; set; }
+        [ForeignKey("Device_statustypes_id")]
+        public virtual Device_statustypes Device_Statustypes { get; set; }
+    }
         [Table("Device_statustypes")]
         public class Device_statustypes
         {
@@ -230,7 +251,7 @@ namespace ConcremoteDeviceManagment.Models
     }
     public class BasDbContext : DbContext
         {
-            //[ForeignKey("Price_id")]
+            [ForeignKey("Price_id")]
             public DbSet<Stock> Stock { get; set; }
             public DbSet<Pricelist> pricelist { get; set; }
             public DbSet<DeviceConfig> DeviceConfig { get; set; }
