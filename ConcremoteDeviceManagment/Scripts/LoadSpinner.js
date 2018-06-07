ba125699da48a899a83e8385d67094f35b133f06
Syracks1 <lclassens@hotmail.com>
2018-06-07 14:22:08 +0200

    Got my Loading Spinny Thing working!

--- ConcremoteDeviceManagment/Scripts/LoadSpinner.js
+++ ConcremoteDeviceManagment/Scripts/LoadSpinner.js
@@ -1,6 +1,7 @@
 ﻿//paste this code under the head tag or in a separate js file.
 // Wait for window load
-$(window).load(function () {
+//$(window).load(function () {
+$(document).ready(function () {
     // Animate loader off screen
     $(".se-pre-con").fadeOut("slow");;
 });
