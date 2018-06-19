7a2ec38f4843b144d57eb98dd74ec0403355d66b
Syracks1 <lclassens@hotmail.com>
2018-06-19 13:21:55 +0200

    Test in a query

--- ConcremoteDeviceManagment/Query's/DeviceSteps-test.sql
+++ ConcremoteDeviceManagment/Query's/DeviceSteps-test.sql
@@ -1,3 +1,4 @@
 ﻿SELECT bas_art_nr, description, amount, assembly_order
 FROM pricelist, Device_Parts, Step_Parts
 WHERE Step_Parts.Step_id = 7;
+/* dit is een test om te kijken of GitHub nog werkt */
